f78cd990635c8d11b0c46d562142a408310d4df7
Christopher Quijano <CQuijano@advantec-usa.com>
2025-09-03 09:32:16 -0700

    Update

--- core/app.py
+++ core/app.py
@@ -452,6 +452,7 @@ with tab1:
 
                 st.caption("Select Origin, Destination, and Date Range")
                 st.caption("Data: Vehicle Speed, Delay, and Travel Time")
+                st.markdown("#### 🗺️ Select Origin and Destination")
 
                 # O-D mode (origin → destination)
                 od_mode = st.checkbox(
